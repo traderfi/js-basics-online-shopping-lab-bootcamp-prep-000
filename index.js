c06814a55a86f9c6346c1ea603d8d1f250a91385
Jay G <playerjayho@gmail.com>
2017-12-01 18:01:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ function viewCart() {
         let j = 0;
 
         console.log(`${Object.keys(cart)} at ${keys}`);
+        console.log(cart[0]);
         j++;
 
       console.log(`In your cart, you have .`);
